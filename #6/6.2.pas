a0c90ad6c521ee8f0f9dbe19c534d94bab87f182
F1lip1n <109240070+F1lip1n@users.noreply.github.com>
2022-07-23 10:56:39 +0300

    Add files via upload

---
+++ #6/6.2.pas
@@ -0,0 +1,41 @@
+﻿type
+  tArr=array[1..100,1..100] of integer;
+var
+  a,AtA: tArr;
+  n,i,j,k,tmp: integer;
+begin
+  write('Введіте розмір матриці: ');
+  readln(n);
+  k:=0;
+  writeln('Вихідна матриця:');
+  for i:=1 to n do
+  begin
+    for j:=1 to n do
+    begin
+      inc(k);
+      a[i,j]:=k;
+      write(a[i,j]:4);
+    end;
+    writeln;
+  end;
+  writeln;
+  
+  AtA:=a;
+  
+  for i:=1 to n do
+    for j:=i+1 to n do
+      begin
+        tmp:=AtA[i,j];
+        AtA[i,j]:=AtA[j,i];
+        AtA[j,i]:=tmp;
+      end;
+      
+  writeln('Отримана матриця:');      
+  for i:=1 to n do
+  begin
+    for j:=1 to n do
+      write(AtA[i,j]:4);
+    writeln;
+  end;
+  readln;
+end.
