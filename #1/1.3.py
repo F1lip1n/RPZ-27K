7cdf60b45093ba7e29d19e626da4a3006e185633
F1lip1n <109240070+F1lip1n@users.noreply.github.com>
2022-07-23 10:42:37 +0300

    Add files via upload

---
+++ #1/1.3.py
@@ -0,0 +1,14 @@
+from random import randint
+
+amount = 10
+numbers = []
+
+for i in range(amount):
+    numbers.append(randint(-50, 50))
+
+print(numbers)
+print(' ')
+print(numbers[1],)
+print(numbers[2], numbers[3])
+print(numbers[4], numbers[5], numbers[6])
+print(numbers[7], numbers[8], numbers[9], numbers[10])
